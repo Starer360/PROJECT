51466a66bdeac239279b3eee9fe4c4cfabd3c407
starer360 <2903197307@qq.com>
2024-08-22 16:06:35 +0800

    02

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calculator 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+}
