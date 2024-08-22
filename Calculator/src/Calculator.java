4e98bb3d5671c57596be1597ad1fa224621dc37c
starer360 <2903197307@qq.com>
2024-08-22 21:50:38 +0800

    04

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -28,8 +28,72 @@ public class Calculator implements ActionListener
         textField = new JTextField();
         textField.setBounds(50, 25, 300, 50);
         textField.setFont(myFont);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
 
 
+        functiButtons[0] = addButton;
+        functiButtons[1] = subButton;
+        functiButtons[2] = mulButton;
+        functiButtons[3] = divButton;
+        functiButtons[4] = decButton;
+        functiButtons[5] = equButton;
+        functiButtons[6] = delButton;
+        functiButtons[7] = clrButton;
+
+        for (int i = 0; i < 8; i++)
+        {
+            functiButtons[i].addActionListener(this);
+            functiButtons[i].setFont(myFont);
+            functiButtons[i].setFocusable(false);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+        
+
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
         frame.add(textField);
         frame.setVisible(true);
 
