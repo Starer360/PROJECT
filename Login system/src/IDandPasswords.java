5545d4b7b5de8b8472cc001de22e378d16298006
starer360 <2903197307@qq.com>
2024-08-23 21:20:14 +0800

    02

--- Login system/src/IDandPasswords.java
+++ Login system/src/IDandPasswords.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 public class IDandPasswords 
 {
-    
+    HashMap<String, String> logininfo = new HashMap<String, String>();
+    IDandPasswords()
+    {
+        logininfo.put("Starer360", "123456");
+        logininfo.put("STAR", "567949");
+        logininfo.put("abc123", "abc123");
+    }
+    protected HashMap getLoginInfo()
+    {
+        return logininfo;
+    }
     
 }
--- Login system/src/Main.java
+++ Login system/src/Main.java
@@ -3,5 +3,7 @@ public class Main
     public static void main(String[] args) throws Exception 
     {
         IDandPasswords iDandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(iDandPasswords.getLoginInfo());
     }
 }
