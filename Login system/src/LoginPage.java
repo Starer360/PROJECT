407c8db0a2564baedb37f82d5227d62a08e145a2
starer360 <2903197307@qq.com>
2024-08-24 22:48:17 +0800

    03

--- Login system/src/LoginPage.java
+++ Login system/src/LoginPage.java
@@ -1,13 +1,57 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 public class LoginPage implements ActionListener
 {
-    
+    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("userID:");
+    JLabel userPasswordJLabel = new JLabel("password:");
+    JLabel messagLabel = new JLabel();
+
     HashMap<String, String> logininfo = new HashMap<String, String>();
     LoginPage(HashMap<String, String> logininfoOriginal)
     {
         logininfo = logininfoOriginal;
+
+        userIDLabel.setBounds(50, 100, 75, 25);
+        userPasswordJLabel.setBounds(50, 150, 75, 25);
+        
+        userIDField.setBounds(125, 100, 200, 25);
+        userPasswordField.setBounds(125, 150, 200, 25);
+
+        messagLabel.setBounds(125, 250, 250, 35);
+        messagLabel.setFont(new Font(null, Font.ITALIC, 25));
+
+
+        loginButton.setBounds(125, 200, 100, 25);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(this);
+
+        resetButton.setBounds(225, 200, 100, 25);
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
+        frame.add(userIDField);
+        frame.add(userPasswordField);
+        frame.add(userIDLabel);
+        frame.add(userPasswordJLabel);
+        frame.add(resetButton);
+        frame.add(loginButton);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) 
