efa2679b5864756ebe7388776d4d417b1ca97ae4
starer360 <2903197307@qq.com>
2024-09-22 20:46:02 +0800

    01

---
+++ Pong game(java)/src/Ball.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball extends Rectangle
+{
+    Random random;
+    int xVelocity;
+    int yVelocity;
+
+    Ball()
+    {
+
+    }
+
+    public void setXDirection(int randomXDirection)
+    {
+
+    }
+
+    public void setYDirection(int randomXDirection)
+    {
+
+    }
+
+    public void move()
+    {
+
+    }
+
+    public void draw(Graphics g)
+    {
+
+    }
+}
